38202feb7c90a8b7b4d229cfc8cac32a94bb03ec
Solano Morales <smorales@panapps.com>
2018-03-08 14:46:39 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: smorales
+ * Date: 08/03/18
+ * Time: 14:38
+ */
+
---
+++ src/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Elo\Api;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: smorales
+ * Date: 08/03/18
+ * Time: 14:22
+ */
+
+class HelloWorld
+{
+	public static function test()
+	{
+		return 'Hello World!';
+	}
+}
