51163003e1ed85997260720dea9762f626a4ce2f
Zeky Gomes <zekygomes@gmail.com>
2019-06-25 18:19:12 -0300

    add getProvisionedUser

---
+++ src/Elo/Api/Cmd/GetProvisionedUser.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: smorales
+ * Date: 10/03/18
+ * Time: 17:52
+ */
+
+namespace Elo\Api\Cmd;
+
+
+use App\catskillet\api\src\Elo\Api\Cmd\VO\ProvisionedUserData;
+use App\catskillet\api\src\Elo\Api\Cmd\VO\UserData;
+
+class GetProvisionedUser extends EloApiCMD
+{
+	/** @var UserData  */
+	private $usercpf;
+
+	public function __construct($usercpf)
+	{
+		parent::__construct();
+		$this->usercpf = $usercpf;
+		$this->isPrivateCall = true;
+		$this->graphQLFile = 'getProvisionedUser';
+	}
+
+	public function execute()
+	{
+        $data = [
+            'cpf'       => $this->usercpf
+        ];
+
+		$this->call($data);
+	}
+}
